4908e9552b0568d017930f0980c16755c20d4983
stefvtls <stefvtls@gmail.com>
2023-03-09 16:37:56 +0100

    FEFACTOR: refactored logout buttons

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import keycloak from 'src/keycloak';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -7,7 +7,34 @@ import keycloak from 'src/keycloak';
   styleUrls: ['./profile.page.css']
 })
 export class ProfilePage {
+  handleLogout(){
+    keycloak.logout();
+   }
 
+  handleDisplayToken() {
+    console.log("USER/SUB ID");
+    console.log("userID", keycloak.tokenParsed?.sub);
+    console.log("username", keycloak.tokenParsed?.preferred_username);
+
+    console.log("PERSONAL DATA")
+    console.log("?name", keycloak.tokenParsed?.name);
+    console.log("last name", keycloak.tokenParsed?.family_name);
+    console.log("first name", keycloak.tokenParsed?.given_name);
+
+    console.log("EMAILING")
+    console.log("email", keycloak.tokenParsed?.email);
+    console.log("verified email: ", keycloak.tokenParsed?.email_verified)
+   
+    console.log("ROLES",keycloak.tokenParsed?.roles );
+    console.log("SCOPE", keycloak.tokenParsed?.scope);
+
+    console.log(keycloak.tokenParsed?.jti);
+    console.log(keycloak.tokenParsed?.sid);
+    console.log(keycloak.tokenParsed?.typ);
+    console.log(keycloak.tokenParsed?.iss);
+
+    console.log("TOKEN", keycloak.token);
+  }
 
 
 }
