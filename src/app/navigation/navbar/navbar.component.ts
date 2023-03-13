9a5c1a37d78782c6c5d90a885f85ecbb207481b7
stefvtls <stefvtls@gmail.com>
2023-03-13 12:10:59 +0100

    FEAT: added HTTP Client Module Dependency

--- src/app/navigation/navbar/navbar.component.ts
+++ src/app/navigation/navbar/navbar.component.ts
@@ -21,12 +21,55 @@ export class NavbarComponent {
   } 
 
 
-  // logs out the current trainer by resetting the trainer data in TrainerService and navigates to login page
+
   handleLogOut() {
     keycloak.logout();
-    // this.trainerService.trainer = undefined;
-    // this.trainerService.clearTrainer();
-    // this.router.navigateByUrl("/login");
+        // clear state in account service
+  }
+
+
+
+  handleLogIn() {
+    keycloak.login();
+    // save a state in account service
+    // check if user exist in our db -> get API with the subID
+  }
+
+
+  handleDisplayToken() {
+    console.log("USER/SUB ID");
+    console.log("userID", keycloak.tokenParsed?.sub);
+    console.log("username", keycloak.tokenParsed?.preferred_username);
+
+    console.log("PERSONAL DATA")
+    console.log("?name", keycloak.tokenParsed?.name);
+    console.log("last name", keycloak.tokenParsed?.family_name);
+    console.log("first name", keycloak.tokenParsed?.given_name);
+
+    console.log("EMAILING")
+    console.log("email", keycloak.tokenParsed?.email);
+    console.log("verified email: ", keycloak.tokenParsed?.email_verified)
+   
+    console.log("REALM ROLES: ", keycloak.tokenParsed?.realm_access);
+    if (keycloak.tokenParsed?.realm_access) {
+      console.log("is admin ", keycloak.hasRealmRole("ADMIN")); 
+      console.log("is user ", keycloak.hasRealmRole("USER")); 
+      keycloak.tokenParsed.realm_access.roles.includes('ADMIN') ? console.log(" I am an admin!") : console.log("");
+      keycloak.tokenParsed.realm_access.roles.includes('USER') ? console.log(" I am a user!") : console.log("");
+      // for (let i = 0; i < keycloak.tokenParsed?.realm_access.roles.length; i++) {
+      //   console.log ("no." + i + keycloak.tokenParsed?.realm_access.roles[i]);
+      // }
+    }
+
+
+    console.log("SCOPE", keycloak.tokenParsed?.scope);
+    console.log("ROLES", keycloak.tokenParsed?.roles);
+    // console.log(keycloak.tokenParsed?.jti);
+    // console.log(keycloak.tokenParsed?.sid);
+    // console.log(keycloak.tokenParsed?.typ);
+    // console.log(keycloak.tokenParsed?.iss);
+
+    console.log("TOKEN", keycloak.token);
   }
 
 
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     TIME_TO_REFRESH_TOKEN_IN_SECONDS: 3600,
-    adminRole: "ADMIN"
+    adminRole: "ADMIN",
+    apiURL: "http://localhost:8080/api/v1/"
 };
