78d7c2b8d4f835c6a44bdd45943169ce997cc72f
stefvtls <stefvtls@gmail.com>
2023-03-13 15:39:45 +0100

    CHORE: testing environments

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 import { initialize } from './keycloak';
 
 
 
-
+console.log("ENVIRONMENT: ", environment.production);
 initialize()
   .then(()=> {
     platformBrowserDynamic()
